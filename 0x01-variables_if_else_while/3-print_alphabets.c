15910e91d218aadb4ed29af10a74a7fb2bd3fff0
Hambugger <aonimis277@gmail.com>
2023-02-17 10:02:08 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: The uppercase and lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+
+		}
+	putchar('\n');
+	return (0);
+}
+
