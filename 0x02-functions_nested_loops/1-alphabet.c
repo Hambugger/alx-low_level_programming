88855361a5f1b50baf382a30689a8e5ed7891fc8
Hambugger <aonimis277@gmail.com>
2023-02-21 05:45:15 -0800

    Print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - Entry point
+ * Description - Lowercase alphabet
+ * Return: void
+ */
+
+void Print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar(\"n);
+}
