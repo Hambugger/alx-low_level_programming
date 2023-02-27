4265c908236e343d8656f010e4e2b4741d3698cd
Hambugger <aonimis277@gmail.com>
2023-02-27 09:14:01 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[1] != '\0')
+	while (str[i] != '\0')
 	{
-		if (1 % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(str[1]);
+			_putchar(str[i]);
 		}
 		i++;
 	}
