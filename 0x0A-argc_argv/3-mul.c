cb8ccb4f38afdbcc8a1fe203b10a903cf8a3b3b9
Hambugger <aonimis277@gmail.com>
2023-03-10 14:16:42 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,72 +1,27 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - Entry point
+ * Description: Multiplies two numbers
+ * @argc: No of command line arguments
+ * @argv: Array name
+ * Return: 1 if needs more arguements, 0 otherwise
  */
 
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- *  main - multiplies two numbers
- *  @argc: number of arguments
- *  @argv: array of arguments
- *
- *  Return: 0 (Success), 1 (Error)
- */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int n1, n2, ans;
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	ans = n1 * n2;
+	printf("%d\n", ans);
 	return (0);
 }
